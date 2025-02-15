a4a8dd3a078a9b228572e24f87370a48d37ef4a1
Anurag dubey <dubeyanurag340@gmail.com>
2025-02-15 23:51:48 +0530

    made some changes in the layout file to dpeloy on vercel

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,43 +1,35 @@
-// import Nav from '@components/Nav';
 import '@styles/globals.css';
 import {dark} from '@clerk/themes';
-// import Provider from "./Provider";
-
 import Nav from '@components/Nav';
-import { ClerkLoaded, ClerkLoading} from '@clerk/nextjs';
-import { ClerkProvider } from '@clerk/nextjs';
-export const metadata={
-  title:'PromptTown',
-  description:'discover & share AI prompts',
+import { ClerkLoaded, ClerkLoading, ClerkProvider } from '@clerk/nextjs';
+
+export const metadata = {
+  title: 'PromptTown',
+  description: 'discover & share AI prompts',
 }
 
-const RootLayout = ( {children} ) => {
-  return(
-    <ClerkProvider appearance={{baseTheme: dark}}>
-     <html lang='en'>
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
       <body>
-       
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-              <Nav/>
+        <ClerkProvider appearance={{baseTheme: dark}}>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav/>
             <ClerkLoading>
               <div className='flex items-center justify-center h-screen text-2xl'>
                 loading...
               </div>
-              
             </ClerkLoading>
             <ClerkLoaded>
               {children}
             </ClerkLoaded>
-            
-       
-       </main>
-    
+          </main>
+        </ClerkProvider>
       </body>
-     </html>
-     </ClerkProvider>
+    </html>
   )
 }
 
